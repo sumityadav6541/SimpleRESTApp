e919ad2d9cd65ac24435827e7f0ea1071635a036
Sumit Yadav <sumityadav6541@gmail.com>
2015-12-26 23:39:53 +0530

    added the PUT Method request

--- src/com/simple/rest/FtoCService.java
+++ src/com/simple/rest/FtoCService.java
@@ -3,6 +3,7 @@ package com.simple.rest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -81,5 +82,22 @@ public class FtoCService {
 		
 		//String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
 		return Response.status(200).entity(jsonObject.toString()).build();
+	  }
+	  
+	  @PUT
+	  @Produces("application/json")
+	  public Response convertFtoCPUT() throws JSONException {
+ 
+		JSONObject jsonObject = new JSONObject();
+		Double fahrenheit = 98.24;
+		Double celsius;
+		celsius = (fahrenheit - 32)*5/9; 
+		jsonObject.put("Method Type", "PUT");
+		jsonObject.put("F Value", fahrenheit); 
+		jsonObject.put("C Value", celsius);
+ 
+		//String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		String result = jsonObject.toString();
+		return Response.status(200).entity(result).build();
 	  }
 }
